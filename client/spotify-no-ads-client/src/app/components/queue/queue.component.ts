9f2c035915aaf7bccb7a75fce9c87a249770aeb9
william_martineau@hotmail.com <william_martineau@hotmail.com>
2021-03-30 21:57:37 -0400

    written empty if queue is empty

--- client/spotify-no-ads-client/src/app/components/queue/queue.component.ts
+++ client/spotify-no-ads-client/src/app/components/queue/queue.component.ts
@@ -2,15 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { MusicPlayerService } from 'src/app/services/music-player/music-player.service';
 
 @Component({
-  selector: 'app-queue',
-  templateUrl: './queue.component.html',
-  styleUrls: ['./queue.component.scss']
+    selector: 'app-queue',
+    templateUrl: './queue.component.html',
+    styleUrls: ['./queue.component.scss']
 })
 export class QueueComponent implements OnInit {
 
-  constructor(public musicPlayerService: MusicPlayerService) { }
+    constructor(public musicPlayerService: MusicPlayerService) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
